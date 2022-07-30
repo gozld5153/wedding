90d768f986010f1e1841aae64174a7666b100269
gozld5153 <gozld5153@naver.com>
2022-07-30 23:37:28 +0900

    greeting page

--- src/pages/Greeting.jsx
+++ src/pages/Greeting.jsx
@@ -6,21 +6,26 @@ export default function Greeting() {
     <Container>
       <ImgBox>
         <img
-          src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbGzWps%2FbtrH9Ivr8Xg%2FBVmd1tg99Ca6G34XoyP0Yk%2Fimg.jpg"
+          src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FV56A0%2FbtrIzE1cyns%2FuxtPWqGMZKTFasNiAszYrK%2Fimg.jpg"
           alt=""
         />
       </ImgBox>
-      <Invite>
-        <p>서로가 마주보며 다져온 사랑을</p>
-        <p>이제 함께 한 곳을 바라보며 걸어갈 수 있는</p>
-        <p>큰 사랑으로 키우고자 합니다.</p>
-        <p>저희 두 사람이 사랑의 이름으로 지켜나갈 수 있게 </p>
-        <p>앞날을 축복해 주시면 감사하겠습니다.</p>
-      </Invite>
-      <FamilyBox>
-        <Family>황운하 &middot; 조석순의 장남 황민환</Family>
-        <Family>장성모 &middot; 김금자의 장녀 장세라</Family>
-      </FamilyBox>
+
+      <Border>
+        <VoidBox />
+        <Invite>
+          <p>서로가 마주보며 다져온 사랑을</p>
+          <p>이제 함께 한 곳을 바라보며 걸어갈 수 있는</p>
+          <p>큰 사랑으로 키우고자 합니다.</p>
+          <p>저희 두 사람이 사랑의 이름으로 지켜나갈 수 있게 </p>
+          <p>앞날을 축복해 주시면 감사하겠습니다.</p>
+        </Invite>
+        <FamilyBox>
+          <Family>황운하 &middot; 조석순의 장남 황민환</Family>
+          <Family>장성모 &middot; 김금자의 장녀 장세라</Family>
+        </FamilyBox>
+      </Border>
+
       <AccountBox>
         <p>마음 보내실 곳</p>
         <p>농협 601131-56-111803 예금주: 황민환</p>
@@ -31,26 +36,38 @@ export default function Greeting() {
 }
 
 const Container = styled.div`
+  position: relative;
   width: 100vw;
   height: calc(var(--vh, 1vh) * 100);
   /* height: 100vh; */
-  font-family: "Song Myung", serif;
+  font-family: "Do Hyeon", sans-serif;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
 `;
 
 const ImgBox = styled.div`
-  width: 60%;
-  height: 40%;
-  margin: 20px auto;
+  position: absolute;
+  width: 100%;
+  height: 40vh;
+  margin: 40px auto;
+  z-index: -1;
 
   img {
     width: 100%;
     height: 100%;
-
-    object-fit: contain;
+    object-fit: cover;
   }
+`;
+
+const Border = styled.div`
+  border: 2px solid #ffe4e1;
+  width: 90%;
+  margin: 20px auto;
+`;
+
+const VoidBox = styled.div`
+  height: calc(40vh + 40px);
 `;
 
 const Invite = styled.div`
@@ -74,4 +91,5 @@ const Family = styled.div`
 const AccountBox = styled.div`
   opacity: 0.6;
   padding: 10px;
+  background-color: #ffe4e1;
 `;
