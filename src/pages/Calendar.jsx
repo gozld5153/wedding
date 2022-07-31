fccf9ed5483b00ac64cdf51cb8ddc5405e703fae
gozld5153 <gozld5153@naver.com>
2022-07-31 12:05:24 +0900

    css change

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -5,62 +5,64 @@ export default function Calendar() {
   return (
     <Container>
       <MiniCalendar>
-        <tr>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td>01</td>
-        </tr>
-        <tr>
-          <td>
-            <div>02</div>
-          </td>
-          <td style={{ color: "#cd5c5c" }}>03</td>
-          <td>04</td>
-          <td>05</td>
-          <td>06</td>
-          <td>07</td>
-          <td>08</td>
-        </tr>
-        <tr>
-          <td>09</td>
-          <td>10</td>
-          <td>11</td>
-          <td>12</td>
-          <td>13</td>
-          <td>14</td>
-          <td>15</td>
-        </tr>
-        <tr>
-          <td>16</td>
-          <td>17</td>
-          <td>18</td>
-          <td>19</td>
-          <td>20</td>
-          <td>21</td>
-          <td>22</td>
-        </tr>
-        <tr>
-          <td>23</td>
-          <td>24</td>
-          <td>25</td>
-          <td>26</td>
-          <td>27</td>
-          <td>28</td>
-          <td>29</td>
-        </tr>
-        <tr>
-          <td>30</td>
-          <td>31</td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-        </tr>
+        <tbody>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>01</td>
+          </tr>
+          <tr>
+            <td>
+              <div>02</div>
+            </td>
+            <td style={{ color: "#cd5c5c" }}>03</td>
+            <td>04</td>
+            <td>05</td>
+            <td>06</td>
+            <td>07</td>
+            <td>08</td>
+          </tr>
+          <tr>
+            <td>09</td>
+            <td>10</td>
+            <td>11</td>
+            <td>12</td>
+            <td>13</td>
+            <td>14</td>
+            <td>15</td>
+          </tr>
+          <tr>
+            <td>16</td>
+            <td>17</td>
+            <td>18</td>
+            <td>19</td>
+            <td>20</td>
+            <td>21</td>
+            <td>22</td>
+          </tr>
+          <tr>
+            <td>23</td>
+            <td>24</td>
+            <td>25</td>
+            <td>26</td>
+            <td>27</td>
+            <td>28</td>
+            <td>29</td>
+          </tr>
+          <tr>
+            <td>30</td>
+            <td>31</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+        </tbody>
       </MiniCalendar>
     </Container>
   );
@@ -84,7 +86,7 @@ const MiniCalendar = styled.table`
     div {
       width: 30px;
       height: 30px;
-      /* line-height: 25px; */
+      line-height: 30px;
       border: 2px solid red;
       border-radius: 100%;
     }
